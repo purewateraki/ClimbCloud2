b8bb3d340910b63cef9fd9e225509607b042655f
purewateraki <60772883+purewateraki@users.noreply.github.com>
2020-02-08 08:32:43 +0900

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // LoadSceneを使うために必要
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 780.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ジャンプする
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            this.rigid2D.velocity.y == 0)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // 左右移動
+        int Key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) Key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) Key = -1;
+
+        // プレイヤの速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * Key * this.walkForce);
+        }
+
+        // 反転対策
+        if (Key != 0)
+        {
+            transform.localScale = new Vector3(Key, 1, 1);
+        }
+
+        // プレイヤの速度に応じてアニメーション速度を変える
+        this.animator.speed = speedx / 2.0f;
+    }
+
+    // ゴールに到達
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
+        
+    }
